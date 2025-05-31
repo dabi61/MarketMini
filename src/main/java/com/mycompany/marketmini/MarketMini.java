4eb85f499a196bb06c6dac6600df4008b106f01d
dabi61 <daodinhbinh612004@gmail.com>
2025-05-31 18:07:08 +0700

    update code

--- src/main/java/com/mycompany/marketmini/MarketMini.java
+++ src/main/java/com/mycompany/marketmini/MarketMini.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.marketmini;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import com.raven.main.Main;
 import java.net.URL;
 import javax.swing.UIManager;
@@ -18,12 +19,12 @@ public class MarketMini {
     public static void main(String[] args) {
         Main view = new Main();
 //        new CRUDController(view);
-//        try {
-//            UIManager.setLookAndFeel(new FlatLightLaf());
-//        } catch (UnsupportedLookAndFeelException ex) {
-//            System.err.println("Không thể thiết lập FlatLaf: " + ex);
-//        }
-        // Gọi giao diện chính sau khi thiết lập LookAndFeel
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException ex) {
+            System.err.println("Không thể thiết lập FlatLaf: " + ex);
+        }
+//         Gọi giao diện chính sau khi thiết lập LookAndFeel
         java.awt.EventQueue.invokeLater(() -> {
             view.setVisible(true); // Thay bằng JFrame chính của bạn
         });
